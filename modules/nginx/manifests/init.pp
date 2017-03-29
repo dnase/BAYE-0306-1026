b2fb83b20b94b761a5038a667d30efd633384811
drewbilee <drewbilee@puppetlabs.vm>
2017-03-29 12:24:52 -0700

    lab 10.1 - code simplification

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,27 +1,23 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0664',
+  }
   package { 'nginx':
     ensure => present,
+  }
+  file { '/var/www':
+    ensure => directory,
   }
   file { 'index.html':
     ensure => file,
     path   => '/var/www/index.html',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/nginx/index.html',
-  }
-  file { '/var/www':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
   file { 'nginx.conf':
     ensure  => file,
     path    => '/etc/nginx/nginx.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
